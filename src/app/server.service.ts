5cd44e3e852b06350c035171459d4aac8c19dbfa
willys_tjiawi <willys@huubap.com>
2018-07-03 23:57:41 +0800

    adjust request header

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,11 +1,13 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class ServerService {
   constructor(private _httpClient: HttpClient) {}
 
   postData(servers: any[]) {
-    return this._httpClient.post('https://test-1-723c2.firebaseio.com/data.json', servers);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json; testing-header');
+    return this._httpClient.post('https://test-1-723c2.firebaseio.com/data.json', servers,
+      {headers: headers});
   }
 }
