69533c017c93322d45b952e37fd22e53e858aa46
willys_tjiawi <willys@huubap.com>
2018-07-23 00:03:25 +0800

    create signup, signin, signout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,4 +68,8 @@ export class AppComponent {
       }
     );
   }
+
+  onSignout() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {ServerService} from './server.service';
+import {RouterModule, Routes} from '@angular/router';
+
 // import { HttpModule } from '@angular/http';     // deprecated from ng4.3
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'sign-up', component: SignupComponent},
+  {path: 'sign-in', component: SigninComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     HttpClientModule
     // HttpModule     // deprecated from ng4.3
   ],
